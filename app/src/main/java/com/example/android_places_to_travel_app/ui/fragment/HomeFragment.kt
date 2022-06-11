61a49b92dfc2513f263b5a7eedb41467ef9d836c
AntonEsipow <esipow.anton99@gmail.com>
2022-06-11 16:25:36 +0300

    Added Attraction.kt model based off the raw JSON file of data.

--- app/src/main/java/com/example/android_places_to_travel_app/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/android_places_to_travel_app/ui/fragment/HomeFragment.kt
@@ -22,6 +22,7 @@ class HomeFragment: BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val recyclerView = binding.recyclerView
     }
 
