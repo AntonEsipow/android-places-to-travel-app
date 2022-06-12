6c55439bde33b8acb982839570b555b553c772c3
AntonEsipow <esipow.anton99@gmail.com>
2022-06-12 21:32:11 +0300

    Added menu and location menu icon for AttractionDetailFragment.kt and Google map Intent to show the location of the place.

--- app/src/main/java/com/example/android_places_to_travel_app/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/android_places_to_travel_app/ui/fragment/AttractionDetailFragment.kt
@@ -1,9 +1,9 @@
 package com.example.android_places_to_travel_app.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.navigation.fragment.navArgs
 import com.example.android_places_to_travel_app.R
 import com.example.android_places_to_travel_app.data.Attraction
@@ -18,6 +18,11 @@ class AttractionDetailFragment: BaseFragment() {
     private val saveArgs: AttractionDetailFragmentArgs by navArgs()
     private val attraction: Attraction by lazy {
         attractions.find {it.id == saveArgs.attractionId}!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -42,6 +47,23 @@ class AttractionDetailFragment: BaseFragment() {
         binding.numberOfFactsTextView.text = "${attraction.facts.size} facts"
         binding.numberOfFactsTextView.setOnClickListener {
             // todo
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_attractions_detail, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menuItemLocation -> {
+                val gmmIntentUri = Uri.parse("geo:${attraction.location.latitude},${attraction.location.longitude}?z=10&q=${attraction.title}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
